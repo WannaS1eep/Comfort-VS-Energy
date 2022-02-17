b9a7ed0a17c197685a592071c04e4ea09f9a85e7
WannaS1eep <maj9801@outlook.com>
2022-02-17 11:37:36 +0100

    basic comfort vs fairness

---
+++ lib/element/participantInfo.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class ParticipantInfo {
+  String email = "";
+  String password = "";
+  String userName = "";
+  String location = "";
+  String noiseLvl = "";
+  int tempStart = 21;
+  int tempEnd = 24;
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'element/tab_views.dart';
+import './cal_tc.dart';
+import 'firebase_options.dart';
+
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  MyAlgorithm myAlgorithm = MyAlgorithm();
+  Timer.periodic(const Duration(seconds: 15), (Timer t){myAlgorithm.calculateATC("2");});
+
+  runApp(const MainTabBar());
+}
+
+class MainTabBar extends StatelessWidget {
+  const MainTabBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "TabBar",
+      home: DefaultTabController(
+        length: items.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('My App'),
+            bottom: TabBar(
+              tabs: items.map((ItemView item) {
+                return Tab(text: item.title, icon: Icon(item.icon));
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+            children: items.map((ItemView item) {
+              return Padding(padding: const EdgeInsets.all(12.0),
+              child: item.view);
+            }).toList()
+          )
+        ),
+      ),
+    );
+  }
+}
+
+
+class ItemView {
+  const ItemView({required this.title, required this.icon, required this.view});
+
+  final String title;
+  final IconData icon;
+  final Widget view;
+}
+
+const List<ItemView> items = <ItemView>[
+  ItemView(title: "User", icon: Icons.account_box, view: RegisterTab()),
+  ItemView(title: "Current State", icon: Icons.content_paste, view: CurrentValueTab(roomNumber: "2",)),
+  ItemView(title: "All data", icon: Icons.table_rows_rounded, view: AllInfoTab()),
+
+];
